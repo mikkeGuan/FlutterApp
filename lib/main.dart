ef95020d7851a02078d062e80c01593718aca061
Ruichao Guan <mikkeguan@gmail.com>
2024-09-12 10:10:43 +0300

    Fonts, Hot reload and stless widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-        title: Text("Home page"),
-      centerTitle: true,
-      backgroundColor: Colors.blueAccent,
-    ),
-    drawer: Drawer(
-      child: Text("test"),
-    ),
-    bottomNavigationBar: BottomAppBar(
-      child: Text("btm"),
-    ),
-    body: Center(
-      child: Text("Hellou"),
-    ),
-    floatingActionButton: FloatingActionButton(onPressed: () {
-      },
-        child: Text("Click me")
-    )
+  home: Home()
+));
 
-    ),
-  )
-);
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Home page"),
+            centerTitle: true,
+            backgroundColor: Colors.indigo[600]
+        ),
+        drawer: Drawer(
+          child: Text("test"),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Text("btm"),
+
+        ),
+        body: Center(
+          child: Text(
+            "Testing",
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 3,
+                color: Colors.black87,
+                fontFamily: 'FascinateInline'
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {
+        },
+          child: Text("Clicked"),
+          backgroundColor: Colors.indigo[400],
+        )
+
+    );
+
+  }
+
+}
+
+
 
